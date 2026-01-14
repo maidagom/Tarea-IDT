be176783063eaeab245da61ea016a7f711d2c7c5
Maida Gómez <maidagompin@gmail.com>
2026-01-13 22:26:19 -0300

    web agregada

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: ["*.html"],
+     theme : {
+                extend : {
+                     colors: { 
+                    },
+                    fontFamily : { 
+                        tx : ['Barlow Semi Condensed', 'sans-serif']
+                    },
+                    fontWeight: {
+                        500: 500,
+                    },
+                }
+            }
+}
